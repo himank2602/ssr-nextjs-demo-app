3275cee2009340ae067e0d948c5c11e23dfe829b
Himank Gupta <himank2602gupta@gmail.com>
2019-04-14 19:31:18 +0530

    Update oldIndex.js

--- pages/oldIndex.js
+++ pages/oldIndex.js
@@ -31,7 +31,7 @@ const Index = props =>(
 
 Index.getInitialProps = async function(){
 
-  const res = await fetch('http://uatapi.justride.in/city');
+  const res = await fetch('');
   const data = await res.json();
   
   return{
